e33540d316151cbb6fca039a530e1d85ea681af2
yogmangela <yogmangela@hotmail.co.uk>
2025-08-05 14:35:34 +0100

    LOCALSTACK

--- main.tf
+++ main.tf
@@ -1,9 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  required_version = ">= 1.0"
+}
+
 provider "aws" {
+  region                      = var.region
   access_key                  = "test"
   secret_key                  = "test"
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
+
   endpoints {
     s3 = "http://localhost:4566"
   }
